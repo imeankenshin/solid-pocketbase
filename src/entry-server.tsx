769c024250da6de7037baeec202fe8f56864f275
LinoRino <omurakenshin@gmai.com>
2023-09-03 11:43:57 +0900

    remove middleware

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -3,9 +3,7 @@ import {
   renderAsync,
   StartServer,
 } from "solid-start/entry-server"
-import { pocketBaseInit } from "./api/pocketbase"
 
 export default createHandler(
-  pocketBaseInit,
   renderAsync((event) => <StartServer event={event} />),
 )
